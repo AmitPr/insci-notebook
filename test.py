ba7db8a59a0f24285182a0d3d1c05ff631baf874
Amit Prasad <17amitprasad@gmail.com>
2020-10-05 16:27:42 -0400

    export to file

--- test.py
+++ test.py
@@ -2,12 +2,21 @@ from traitlets.config import Config
 import nbformat
 from nbconvert import HTMLExporter
 import pathlib
+
+c = Config()
+
+#Add this directory to the path that nbconvert searches for templates in
 PATH = pathlib.Path(__file__).parent.absolute()
-print(HTMLExporter.extra_template_basedirs,PATH)
-HTMLExporter.extra_template_basedirs=[PATH]
+c.HTMLExporter.extra_template_basedirs=[PATH]
+
+#Read in example
 with open('example.ipynb','r+') as f:
     notebook = nbformat.reads('\n'.join(f.readlines()),as_version=nbformat.NO_CONVERT)
-html_exporter = HTMLExporter()
+
+html_exporter = HTMLExporter(config=c)
 html_exporter.template_name = 'test'
 (body, resources) = html_exporter.from_notebook_node(notebook)
-print(body)
+
+#Export output to file
+with open('example-out.html','w+') as f:
+    f.write(body)
